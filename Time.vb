6571130b3a7c433f68d135892bda521c417e7b81
Alexey-Pugachev-21IS <123510473+Alexey-Pugachev-21IS@users.noreply.github.com>
2023-03-09 08:35:26 +0300

    Add files via upload

--- Time.vb
+++ Time.vb
@@ -4,12 +4,12 @@ Imports System.Transactions
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Enter number of seconds:")
-        Dim input, hrs, nado, min, sec As Decimal
+        Dim input, hrs, ostatok, min, sec As Decimal
         input = Console.ReadLine()
         hrs = input \ 3600
-        nado = input Mod 3600
-        min = nado \ 60
-        sec = nado Mod 60
+        ostatok = input Mod 3600
+        min = ostatok \ 60
+        sec = ostatok Mod 60
         Console.WriteLine("{0} seconds is: {1} hour(s), {2} minute(s), {3} second(s)", input, hrs, min, sec)
     End Sub
 End Module
