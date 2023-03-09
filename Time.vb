24030e9af3f66c5f9d7f31e0b407f9d7bb49e1fd
Alexey-Pugachev-21IS <123510473+Alexey-Pugachev-21IS@users.noreply.github.com>
2023-03-09 08:31:02 +0300

    Add files via upload

--- Time.vb
+++ Time.vb
@@ -8,8 +8,8 @@ Module Program
         input = Console.ReadLine()
         hrs = input \ 3600
         nado = input Mod 3600
-        min = nado \ 60                            'Без вашей утренней подсказки это сделать нереально, я вчера до 2 часов сидел
-        sec = min Mod 60
+        min = nado \ 60
+        sec = nado Mod 60
         Console.WriteLine("{0} seconds is: {1} hour(s), {2} minute(s), {3} second(s)", input, hrs, min, sec)
     End Sub
 End Module
