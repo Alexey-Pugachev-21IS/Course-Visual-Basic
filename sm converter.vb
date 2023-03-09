f619d67c341600507014511115f85bd6a79b0b0a
Alexey-Pugachev-21IS <123510473+Alexey-Pugachev-21IS@users.noreply.github.com>
2023-03-09 22:39:24 +0300

    Add files via upload

---
+++ sm converter.vb
@@ -0,0 +1,13 @@
+Imports System
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Enter number of seconds:")
+        Dim sm, dm, ostatok, m, input As Decimal
+        input = Console.ReadLine()
+        m = input \ 100
+        ostatok = input Mod 100
+        dm = ostatok \ 10
+        sm = ostatok Mod 10
+        Console.WriteLine("{0} sm is: {1} m, {2} dm, {3} sm", input, m, dm, sm)
+    End Sub
+End Module
