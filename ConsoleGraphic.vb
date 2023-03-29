76cb1e94231ea867c227cf1130df3f469fcde67f
Alexey-Pugachev-21IS <123510473+Alexey-Pugachev-21IS@users.noreply.github.com>
2023-03-30 01:38:20 +0300

    Add files via upload

--- ConsoleGraphic.vb
+++ ConsoleGraphic.vb
@@ -2,11 +2,11 @@ Imports System
 Imports System.Security.Cryptography.X509Certificates
 
 Module ConsoleGraphic
-    Dim input, input1 As Single
-    Dim x, y, x1, y1 As Integer
+    Dim input, input1, input2 As Single
+    Dim x, y, x1, y1, cl As Integer
     Private brush As Char = "0"c
     Sub Main(args As String())
-        Console.WriteLine("0 - Hline, 1 - Vline, 2 - закрашенный прямоугольник, 3 - пустой прямоугольник")
+        Console.WriteLine("0 - Hline, 1 - Vline, 2 - закрашенный прямоугольник, 3 - пустой прямоугольник, 4 - cell ")
         input = Console.ReadLine()
         Console.WriteLine("Хотите изменить построение? 1 - да, 2 - нет.")
         input1 = Console.ReadLine()
@@ -35,6 +35,12 @@ Module ConsoleGraphic
                     Pustcub(x, y, x1, y1)
                 Else
                     Pustcub(10, 10, 20, 10)
+                End If
+            Case 4
+                If input1 = 1 Then
+                    cell(x, y, x1, y1)
+                Else
+                    cell(7, 7, 50, 7)
                 End If
         End Select
     End Sub
@@ -73,6 +79,28 @@ Module ConsoleGraphic
             Console.Write(brush)
         Next
     End Sub
+    Sub cell(leftx As Byte, topy As Byte, width As Integer, Cells As Integer)
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.BackgroundColor = ConsoleColor.DarkGray
+        Console.SetCursorPosition(leftx, topy)
+        For i = 2 To width
+            Console.Write(brush)
+        Next
+        For j = 1 To Cells
+            cl = Console.GetCursorPosition.Top
+            For m1 = 1 To 4
+                Console.SetCursorPosition(leftx, topy + m1)
+                For m2 = 1 To width / 10
+                    Console.Write(brush)
+                    Console.SetCursorPosition(Console.GetCursorPosition.Left + 11, Console.GetCursorPosition.Top)
+                Next
+            Next
+            Console.SetCursorPosition(leftx, topy + 5)
+            For i = 2 To width
+                Console.Write(brush)
+            Next
+        Next
+    End Sub
 
     Sub nastroika()
         If input1 = 1 Then
@@ -84,6 +112,10 @@ Module ConsoleGraphic
             x1 = Console.ReadLine()
             If input = 2 Or input = 3 Then
                 Console.Write("высота = ")
+                y1 = Console.ReadLine()
+            End If
+            If input = 4 Then
+                Console.Write("клеток по вертикали = ")
                 y1 = Console.ReadLine()
             End If
         End If
