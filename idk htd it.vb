86af08e861645703dc62c901d2610653b3e2280d
Alexey-Pugachev-21IS <123510473+Alexey-Pugachev-21IS@users.noreply.github.com>
2023-04-20 08:42:52 +0300

    Add files via upload

--- idk htd it.vb
+++ idk htd it.vb
@@ -7,7 +7,7 @@ Module Program
         x = 2.1
         y = -1.7
         a = (Math.Cos((Math.Abs(x * y))) ^ 3) - 1
-        b = Math.E ^ x * y - Math.Abs(y) * Math.Sin(x)
+        b = Math.E ^ (x * y) - Math.Abs(y) * Math.Sin(x)
         p = a * Math.Log((Math.Abs(b)) - 3.1 * Math.Cos(b)) / Math.Sqrt(Math.Abs(x + (a ^ 2 * b)))
         Console.WriteLine("p = {0}", p)
     End Sub
